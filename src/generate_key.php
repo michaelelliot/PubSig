cb9594b10b68bea4431e154f94e771ff2efcd360
thermal <michael@digital.com.au>
2011-02-26 00:01:35 +0800

    first commit

---
+++ src/generate_key.php
@@ -0,0 +1,15 @@
+<?php
+
+include("pubsig.class.php");
+
+$private_key_filename = str_replace("\\", "/", dirname(__FILE__)) . "/keys/private_key.pem";
+$private_key_passphrase = "abc123";
+
+$ps = new PubSig();
+//$ps->generate_keys(4096);
+//$ps->save_private_key_to_file($private_key_filename, $private_key_passphrase);
+if (!$ps->load_private_key_file($private_key_filename, $private_key_passphrase)) die("Error loading private key from file");
+
+//$cert = new JCert();
+
+?>
---
+++ src/pubsig.class.php
@@ -0,0 +1,31 @@
+<?php
+
+class PubSig {
+    var $public_key;
+    var $key_handle;
+    var $key_details;
+
+    function generate_keys($bits) {
+
+        // Generate a $bits length RSA private key
+        $this->key_handle = openssl_pkey_new(array(
+            'private_key_bits' => $bits,
+            'private_key_type' => OPENSSL_KEYTYPE_RSA,
+        ));
+        // Get the public key from the private key;
+        $this->key_details = openssl_pkey_get_details($this->key_handle);
+        $this->public_key = $this->key_details['key'];
+    }
+    function save_private_key_to_file($private_key_filename, $private_key_passphrase) {
+
+        return openssl_pkey_export_to_file($this->key_handle, $private_key_filename, $private_key_passphrase);
+    }
+    function load_private_key_file($private_key_filename, $private_key_passphrase) {
+
+        return ($this->key_handle = openssl_pkey_get_private("file://$private_key_filename", $private_key_passphrase)) ? $this->key_handle : false;
+    }
+
+//file_put_contents('publickey', $keyDetails['key']);
+}
+
+?>
