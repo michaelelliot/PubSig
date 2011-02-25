014baae6a647d19ba5ed6877507bee3b1777c53e
thermal <michael@digital.com.au>
2011-02-26 01:02:45 +0800

    Added manager files

---
+++ src/manager.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-language" content="en" />
+        <link type="text/css" rel="stylesheet" href="css/styles.css" />
+        <title></title>
+    </head>
+    <body>
+        <img src="images/logo.png" /><br />
+        <div id="header-bar"></div>
+        <table cellpadding="0" cellspacing="0">
+            <tr>
+                <td align="left" valign="top" width="300">
+                    <div class="menu">
+                        <ul>
+                            <li class="selected">Home</li>
+                            <li><a id="" href="#">Messages (1)</a></li>
+                            <li><a id="menu_item_2" href="#">Create New Certificate</a></li>
+                            <li><a href="#">Sign a Certificate</a></li>
+                            <li><a href="#">Signing Authorities</a></li>
+                        </ul>
+                    </div>
+                </td>
+                <td align="left" valign="top">
+                    <div style="height: 1000px;"></div>
+                </td>
+            </tr>
+        </table>
+
+
+            
+            
+        
+        
+    </body>
+</html>
