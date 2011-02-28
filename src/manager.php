92d2eba067c7eb806119cd4476d10a17fbaddd5c
thermal <michael@digital.com.au>
2011-02-28 15:14:10 +0800

    Misc

--- src/manager.php
+++ src/manager.php
@@ -6,6 +6,28 @@
         <meta http-equiv="Content-language" content="en" />
         <link type="text/css" rel="stylesheet" href="css/styles.css" />
         <title></title>
+
+        <script type="text/javascript" src="jscript/jquery-1.3.2.min.js"></script>
+
+        <script type="text/javascript">
+
+            $(document).ready(function() {
+
+                $("#is_signing_authority").change(function() {
+
+                $("#json_rpc_url").closest("tr").css("visibility", this.checked ? "visible" : "hidden");
+                      //     = true;//(this.checked);
+                    //(this.checked ? this.disabled(true) : this.disabled(fal)
+                });
+            });
+
+
+
+
+
+
+
+        </script>
     </head>
     <body>
         <img src="images/logo.png" /><br />
@@ -31,26 +53,28 @@
                             <p>This page allows you to create a new certificate along with a newly generated key pair. This can be used to create certificates for others or to setup the initial certificate that will be used by this Signing Authority.</p>
 
                             <form>
-                                <div class="form">
+                                <div class="outline">
                                     <h3>Certificate Fields</h3>
-                                    <table cellpadding="0" cellspacing="0">
-                                        <tr>
-                                            <td align="left" valign="middle" nowrap class="first">Name:</td>
-                                            <td align="left" valign="middle" width="300"><input type="text" name="name" value="" /></td>
-                                        </tr>
-                                        <tr>
-                                            <td align="left" valign="middle" class="first">Comment:</td>
-                                            <td align="left" valign="middle"><input type="text" name="comment" value="" /></td>
-                                        </tr>
-                                        <tr>
-                                            <td align="left" valign="middle" class="first">JSON-RPC URL:</td>
-                                            <td align="left" valign="middle"><input type="text" name="json_rpc_url" value="" /></td>
-                                        </tr>
-                                    </table>
+                                    <div class="body">
+                                        <table cellpadding="0" cellspacing="0">
+                                            <tr>
+                                                <td align="left" valign="middle" nowrap class="first">Name:</td>
+                                                <td align="left" valign="middle" width="300"><input type="text" name="name" value="" /></td>
+                                            </tr>
+                                            <tr>
+                                                <td align="left" valign="middle" class="first">Comment:</td>
+                                                <td align="left" valign="middle"><input type="text" name="comment" value="" /></td>
+                                            </tr>
+                                            <tr>
+                                                <td align="left" valign="middle" class="first">JSON-RPC URL:</td>
+                                                <td align="left" valign="middle"><input type="text" id="json_rpc_url" name="json_rpc_url" value="" /></td>
+                                            </tr>
+                                        </table>
 
-                                    <p>
-                                        <input type="checkbox" id="is_signing_authority" name="is_signing_authority" /><label for="is_signing_authority">This certificate is for use as a Signing Authority</label>
-                                    </p>
+                                        <p>
+                                            <input type="checkbox" id="is_signing_authority" name="is_signing_authority" /><label for="is_signing_authority">This certificate will be used by a Signing Authority</label>
+                                        </p>
+                                    </div>
                                 </div>
                             </form>
 
